28031bb7a26d079f43cf59236b306daff179526e
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2024-09-21 13:18:15 -0500

    Update Rastreo.jsx

--- src/auth/componentes/Rastreo.jsx
+++ src/auth/componentes/Rastreo.jsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react'
+import { Form, Link } from 'react-router-dom';
 
 export const Rastreo = () => {
     const [trackingNumber, setTrackingNumber] = useState('');
-    const [showIframe, setShowIframe] = useState(false);
+   
 
     const handleInputChange = (e) => {
         setTrackingNumber(e.target.value);
     };
 
-    const handleTrack = () => {
-        if (trackingNumber) {
-            setShowIframe(true);
-        }
-    };
+
+
+    const trackeo=()=>{
+        window.location.href = `https://clientes.japslogistics.com/ConsultaTracking.aspx?cid=1&idGuia=${trackingNumber}`
+
+    }
     return (
         <div className='centrar'>
 
@@ -21,23 +23,16 @@ export const Rastreo = () => {
 
             <div className="tracking-container">
                 <h2>Rastrea tu paquete</h2>
-                <input
-                    type="text"
-                    value={trackingNumber}
-                    onChange={handleInputChange}
-                    placeholder="Ingresa el número de seguimiento"
-                />
-                <button className='btn btn-danger form-controlw-20' onClick={handleTrack}>Rastrear</button>
 
-                {showIframe && (
-                    <iframe
-                        src={`https://www.logisticaxpress.com/tracking?tracking=${trackingNumber}`}
-                        width="100%"
-                        height="600px"
-                        title="Tracking"
-                        style={{ border: 'none' }}
+                
+                    <input
+                        type="text"
+                        value={trackingNumber}
+                        onChange={handleInputChange}
+                        placeholder="Ingresa el número de seguimiento"
                     />
-                )}
+                    <button className='btn btn-danger form-controlw-20' onClick={trackeo}>Rastrear</button>
+                
             </div>
         </div>
     )
